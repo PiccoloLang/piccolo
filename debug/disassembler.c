d61613851c566916abea658614c4a3735382cf43
Nikos Plugachev <nikos@plugachev.com>
2021-10-02 09:50:21 -0400

    Fixed dissasembler

--- debug/disassembler.c
+++ debug/disassembler.c
@@ -8,45 +8,45 @@ static uint16_t getInstructionParam(struct piccolo_Bytecode* bytecode, int offse
 
 int piccolo_disassembleInstruction(struct piccolo_Bytecode* bytecode, int offset) {
 #define SIMPLE_INSTRUCTION(opcode)       \
-    case opcode: {                       \
+    case PICCOLO_ ## opcode: {           \
         printf(#opcode "\n");            \
         return offset + 1;               \
     }
 #define PARAM_INSTRUCTION(opcode)        \
-    case opcode: {                       \
+    case PICCOLO_ ## opcode: {           \
         printf(#opcode " %d\n", getInstructionParam(bytecode, offset)); \
         return offset + 3;               \
     }
 
     printf("%4d | ", offset);
     switch(bytecode->code.values[offset]) {
-        SIMPLE_INSTRUCTION(PICCOLO_OP_RETURN)
+        SIMPLE_INSTRUCTION(OP_RETURN)
         case PICCOLO_OP_CONST: {
-            printf("PICCOLO_OP_CONST { ");
+            printf("OP_CONST { ");
             piccolo_printValue(bytecode->constants.values[getInstructionParam(bytecode, offset)]);
             printf(" }\n");
             return offset + 3;
         }
-        SIMPLE_INSTRUCTION(PICCOLO_OP_ADD)
-        SIMPLE_INSTRUCTION(PICCOLO_OP_SUB)
-        SIMPLE_INSTRUCTION(PICCOLO_OP_MUL)
-        SIMPLE_INSTRUCTION(PICCOLO_OP_DIV)
+        SIMPLE_INSTRUCTION(OP_ADD)
+        SIMPLE_INSTRUCTION(OP_SUB)
+        SIMPLE_INSTRUCTION(OP_MUL)
+        SIMPLE_INSTRUCTION(OP_DIV)
 
-        SIMPLE_INSTRUCTION(PICCOLO_OP_EQUAL)
-        SIMPLE_INSTRUCTION(PICCOLO_OP_GREATER)
-        SIMPLE_INSTRUCTION(PICCOLO_OP_LESS)
-        SIMPLE_INSTRUCTION(PICCOLO_OP_NOT)
+        SIMPLE_INSTRUCTION(OP_EQUAL)
+        SIMPLE_INSTRUCTION(OP_GREATER)
+        SIMPLE_INSTRUCTION(OP_LESS)
+        SIMPLE_INSTRUCTION(OP_NOT)
 
-        SIMPLE_INSTRUCTION(PICCOLO_OP_POP_STACK)
+        SIMPLE_INSTRUCTION(OP_POP_STACK)
 
-        PARAM_INSTRUCTION(PICCOLO_OP_GET_GLOBAL)
-        PARAM_INSTRUCTION(PICCOLO_OP_GET_STACK)
-        SIMPLE_INSTRUCTION(PICCOLO_OP_SET)
+        PARAM_INSTRUCTION(OP_GET_GLOBAL)
+        PARAM_INSTRUCTION(OP_GET_STACK)
+        SIMPLE_INSTRUCTION(OP_SET)
 
-        PARAM_INSTRUCTION(PICCOLO_OP_JUMP)
-        PARAM_INSTRUCTION(PICCOLO_OP_JUMP_FALSE)
+        PARAM_INSTRUCTION(OP_JUMP)
+        PARAM_INSTRUCTION(OP_JUMP_FALSE)
 
-        PARAM_INSTRUCTION(PICCOLO_OP_CALL)
+        PARAM_INSTRUCTION(OP_CALL)
     }
     printf("Unknown Opcode.\n");
     return offset + 1;
